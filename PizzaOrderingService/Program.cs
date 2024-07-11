f66a25bb298cb1732001ff218ba8eeebbfeee4ac
TSRashid <mrrashid3255@gmail.com>
2024-07-11 15:29:18 +0500

    Removed Unused Imports from Program.cs

--- PizzaOrderingService/Program.cs
+++ PizzaOrderingService/Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
-class Program
+﻿class Program
 {
     static SalesStatistics salesStatistics = new SalesStatistics();
     static int currentOrderNumber = 1;
